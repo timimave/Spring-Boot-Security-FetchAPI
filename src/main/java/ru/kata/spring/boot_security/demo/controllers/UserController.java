95b6080345769eca30e9f39ad6acfc4e227af1da
TIm IM <timimave@gmail.com>
2023-03-18 01:37:21 +0500

    14 commit - analysis of trifles

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
@@ -24,12 +26,23 @@ public class UserController {
         this.userService = userService;
     }
     @GetMapping("/user")
-    public String userAccount(Model model, Authentication authentication) {
+    public String userAccount(Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         User user = userService.getUserByName(userDetails.getUsername());
         model.addAttribute("user", user);
+        redirectAttributes.addAttribute("from", "admin");
         return "userinfo/user";
+
     }
+
+
+
 }
 
-
+//@GetMapping("/admin")
+//public String adminAccount(Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
+//    UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+//    User user = userService.getUserByName(userDetails.getUsername());
+//    redirectAttributes.addAttribute("from", "admin");
+//    return "redirect:/user";
+//}
