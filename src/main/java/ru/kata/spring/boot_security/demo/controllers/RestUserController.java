d2c65d238bbb8d35032efb6e60b07ae74548a6c9
TIm IM <timimave@gmail.com>
2023-03-25 04:04:15 +0500

    14 commit

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/RestUserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/RestUserController.java
@@ -1,5 +1,26 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.model.UserDTO;
+import ru.kata.spring.boot_security.demo.services.UserService;
+@RestController
 public class RestUserController {
+    @Autowired
+    private UserService userService;
+    @GetMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDTO> getUser(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        User user = userService.getUserByName(userDetails.getUsername());
+        UserDTO userDTO = new UserDTO(user.getId(),user.getName(),user.getLastname(),
+            user.getPersonWhoStudiesJava(), user.getUsername(),user.getRoles());
+        return ResponseEntity.ok(userDTO);
+    }
 
 }
---
+++ src/main/java/ru/kata/spring/boot_security/demo/model/UserDTO.java
@@ -0,0 +1,74 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import java.util.Set;
+
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String lastname;
+    private String PersonWhoStudiesJava;
+   // private String password;
+    private String username;
+    private Set<Role> roles;
+
+
+    public UserDTO(Long id, String name, String lastname, String personWhoStudiesJava, String username,
+        Set<Role> roles) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        PersonWhoStudiesJava = personWhoStudiesJava;
+        this.username = username;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPersonWhoStudiesJava() {
+        return PersonWhoStudiesJava;
+    }
+
+    public void setPersonWhoStudiesJava(String personWhoStudiesJava) {
+        PersonWhoStudiesJava = personWhoStudiesJava;
+    }
+
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
